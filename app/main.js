796324453888986ce1763aa59e907c5bcf90d9f3
Huy Phan <phan.q.huy21@gmail.com>
2024-05-08 16:37:11 -0400

    404 response

--- app/main.js
+++ app/main.js
@@ -5,7 +5,16 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n")
+    socket.on("data", (data) => {
+        const request = data.toString().split("\r\n")
+        const path = request[0].split(" ")[1]
+
+        if (path === "/") {
+            socket.write("HTTP/1.1 200 OK\r\n\r\n")
+        } else {
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+        }
+    })
 
     socket.on("close", () => {
         socket.end();
