c9042e0c2d361b13aa0809a47627310c5e8976a0
Huy Phan <phan.q.huy21@gmail.com>
2024-05-09 04:06:19 -0400

    POST a file

--- app/main.js
+++ app/main.js
@@ -18,23 +18,37 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
     socket.on("data", (data) => {
-        // Status line
-        const httpVersion = "HTTP/1.1";
-        const successCode = "200 OK";
-        const notFoundCode = "404 Not Found";
-
-        // Headers
+        // Status Codes
+        const res_200 = "HTTP/1.1 200 OK";
+        const res_201 = "HTTP/1.1 201 Created";
+        const res_404 = "HTTP/1.1 404 Not Found";
+        const res_500 = "HTTP/1.1 500 Internal Server Error";
 
         // Response Body
-
         const request = data.toString().split("\r\n");
+        console.log(`This is the request: ${request}`);
         const path = request[0].split(" ")[1];
+        const httpMethod = request[0].split(" ")[0];
         const contentParseArray = path.split("/");
         const content = contentParseArray[contentParseArray.length - 1];
 
+        let response = "";
+
         if (path === "/") {
-            socket.write(`${httpVersion} ${successCode}\r\n\r\n`);
-        } else if (path.includes("files")) {
+            response = `${res_200}\r\n\r\n`;
+        } else if (httpMethod === "POST" && path.includes("files")) {
+            const fileIDX = path.split("/").indexOf("files") + 1;
+            const fileName = path.split("/")[fileIDX];
+            const dirIDX = process.argv.indexOf("--directory");
+            const dir = process.argv[dirIDX + 1];
+            const filePath = p.join(dir, fileName);
+            const contentType = "application/octet-stream";
+
+            console.log(`req body: ${request[request.length - 1]}`);
+
+            fs.writeFileSync(filePath, request[request.length - 1]);
+            response = `${res_201}\r\n\r\n`;
+        } else if (httpMethod === "GET" && path.includes("files")) {
             const fileIDX = path.split("/").indexOf("files") + 1;
             const fileName = path.split("/")[fileIDX];
             const dirIDX = process.argv.indexOf("--directory");
@@ -44,34 +58,24 @@ const server = net.createServer((socket) => {
 
             if (fs.existsSync(filePath)) {
                 const fileContent = fs.readFileSync(filePath, "utf-8");
-                socket.write(
-                    `${httpVersion} ${successCode}\r\nContent-Type: ${contentType}\r\nContent-Length: ${fileContent.length}\r\n\r\n${fileContent}`
-                );
+                response = `${res_200}\r\nContent-Type: ${contentType}\r\nContent-Length: ${fileContent.length}\r\n\r\n${fileContent}`;
             } else {
-                socket.write(
-                    `${httpVersion} ${notFoundCode}\r\nContent-Type: ${contentType}\r\nContent-Length: 0\r\n\r\n`
-                );
+                response = `${res_404}\r\nContent-Type: ${contentType}\r\nContent-Length: 0\r\n\r\n`;
             }
         } else if (path.includes("user-agent")) {
             const headers = request[2];
             const userAgentHeader = request[2].split(": ")[1];
 
-            socket.write(
-                `${httpVersion} ${successCode}\r\nContent-Type: text/plain\r\nContent-Length: ${userAgentHeader.length}\r\n\r\n${userAgentHeader}`
-            );
+            response = `${res_200}\r\nContent-Type: text/plain\r\nContent-Length: ${userAgentHeader.length}\r\n\r\n${userAgentHeader}`;
         } else if (path.includes("echo")) {
-            socket.write(
-                `${httpVersion} ${successCode}\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`
-            );
+            response = `${res_200}\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
         } else {
-            socket.write(`${httpVersion} ${notFoundCode}\r\n\r\n`);
+            response = `${res_404}\r\n\r\n`;
         }
-    });
 
-    // socket.on("close", () => {
-    //     socket.end();
-    //     server.close();
-    // });
+        console.log(response);
+        socket.write(response);
+    });
 });
 
 server.listen(4221, "localhost");
