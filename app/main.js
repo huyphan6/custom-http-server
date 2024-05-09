26292b8ee05184350ae948dde3e00a56b08103ff
Huy Phan <phan.q.huy21@gmail.com>
2024-05-09 02:37:22 -0400

    concurrent connections

--- app/main.js
+++ app/main.js
@@ -17,37 +17,41 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
     socket.on("data", (data) => {
         // Status line
-        const httpVersion = "HTTP/1.1"
-        const successCode = "200 OK"
-        const notFoundCode = "404 Not Found"
+        const httpVersion = "HTTP/1.1";
+        const successCode = "200 OK";
+        const notFoundCode = "404 Not Found";
 
         // Headers
 
         // Response Body
 
-        const request = data.toString().split("\r\n")
-        const path = request[0].split(" ")[1]
-        const contentParseArray = path.split("/")
-        const content = contentParseArray[contentParseArray.length - 1]
+        const request = data.toString().split("\r\n");
+        const path = request[0].split(" ")[1];
+        const contentParseArray = path.split("/");
+        const content = contentParseArray[contentParseArray.length - 1];
 
         if (path === "/") {
-            socket.write(`${httpVersion} ${successCode}\r\n\r\n`)
+            socket.write(`${httpVersion} ${successCode}\r\n\r\n`);
         } else if (path.includes("user-agent")) {
-            const headers = request[2]
-            const userAgentHeader = request[2].split(": ")[1]
+            const headers = request[2];
+            const userAgentHeader = request[2].split(": ")[1];
 
-            socket.write(`${httpVersion} ${successCode}\r\nContent-Type: text/plain\r\nContent-Length: ${userAgentHeader.length}\r\n\r\n${userAgentHeader}`)
+            socket.write(
+                `${httpVersion} ${successCode}\r\nContent-Type: text/plain\r\nContent-Length: ${userAgentHeader.length}\r\n\r\n${userAgentHeader}`
+            );
         } else if (path.includes("echo")) {
-            socket.write(`${httpVersion} ${successCode}\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`)
+            socket.write(
+                `${httpVersion} ${successCode}\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`
+            );
         } else {
-            socket.write(`${httpVersion} ${notFoundCode}\r\n\r\n`)
+            socket.write(`${httpVersion} ${notFoundCode}\r\n\r\n`);
         }
-    })
-
-    socket.on("close", () => {
-        socket.end();
-        server.close();
     });
+
+    // socket.on("close", () => {
+    //     socket.end();
+    //     server.close();
+    // });
 });
 
 server.listen(4221, "localhost");
